8e2a17de45c822c6f9cd5bac81df94b35741107a
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-14 15:13:18 -0300

    Usuario - create

--- src/main/java/com/yasmingv/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/yasmingv/demoparkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.yasmingv.demoparkapi.service;
 
+import com.yasmingv.demoparkapi.entity.Usuario;
 import com.yasmingv.demoparkapi.repository.Usuariorepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +11,9 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final Usuariorepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
 }
--- src/main/java/com/yasmingv/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/yasmingv/demoparkapi/web/controller/UsuarioController.java
@@ -1,14 +1,23 @@
 package com.yasmingv.demoparkapi.web.controller;
 
+import com.yasmingv.demoparkapi.entity.Usuario;
 import com.yasmingv.demoparkapi.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
+@RequiredArgsConstructor 
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+    @PostMapping
+    private ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+      Usuario user =  usuarioService.salvar(usuario);
+      return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
+
 }
