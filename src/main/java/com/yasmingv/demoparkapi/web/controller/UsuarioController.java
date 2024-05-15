7ba49a25552413e4222c7ba46ec18e68bf2dac4c
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-15 11:12:14 -0300

    Validando obj UsuarioCreateDto

--- src/main/java/com/yasmingv/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/com/yasmingv/demoparkapi/web/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import com.yasmingv.demoparkapi.web.dto.UsuarioCreateDto;
 import com.yasmingv.demoparkapi.web.dto.UsuarioResponseDto;
 import com.yasmingv.demoparkapi.web.dto.UsuarioSenhaDto;
 import com.yasmingv.demoparkapi.web.dto.mapper.UsuarioMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,7 +22,7 @@ public class UsuarioController {
     private final UsuarioService usuarioService;
 
     @PostMapping
-    private ResponseEntity<UsuarioResponseDto> create(@RequestBody UsuarioCreateDto createDto) {
+    private ResponseEntity<UsuarioResponseDto> create(@Valid @RequestBody UsuarioCreateDto createDto) {
       Usuario user =  usuarioService.salvar(UsuarioMapper.toUsuario(createDto));
       return ResponseEntity.status(HttpStatus.CREATED).body(UsuarioMapper.toDto(user));
     }
