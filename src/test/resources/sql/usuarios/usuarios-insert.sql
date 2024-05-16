fbfba942a2bcbf5951ae4ff12575e58e2b2f47a7
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-16 10:55:59 -0300

    Test criarUsuario

---
+++ src/test/resources/sql/usuarios/usuarios-insert.sql
@@ -0,0 +1,3 @@
+INSERT INTO USUARIOS (id, username, password, role) VALUES (100, 'ana@gmail.com', '666333', 'ROLE_ADMIN');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (101, 'bia@gmail.com', '266333', 'ROLE_CLIENTE');
+INSERT INTO USUARIOS (id, username, password, role) VALUES (102, 'bob@gmail.com', '626333', 'ROLE_CLIENTE');
