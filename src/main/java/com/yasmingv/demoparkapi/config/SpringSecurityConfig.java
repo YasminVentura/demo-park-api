8e6774f142b839da68be7c8415e79a6832bcbac0
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-17 18:01:56 -0300

    Tratando erro 401 e 403

--- src/main/java/com/yasmingv/demoparkapi/config/SpringSecurityConfig.java
+++ src/main/java/com/yasmingv/demoparkapi/config/SpringSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.yasmingv.demoparkapi.config;
 
+import com.yasmingv.demoparkapi.jwt.JwtAuthenticationEntryPoint;
 import com.yasmingv.demoparkapi.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,6 +35,8 @@ public class SpringSecurityConfig {
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(
                         jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+                ).exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 ).build();
     }
 
