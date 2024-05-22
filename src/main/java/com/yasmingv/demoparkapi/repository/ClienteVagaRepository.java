06149a6af97d0ecedb56f80ae871208139bc5818
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-22 16:17:39 -0300

    ClienteVaga - repository, sevrice

---
+++ src/main/java/com/yasmingv/demoparkapi/repository/ClienteVagaRepository.java
@@ -0,0 +1,8 @@
+package com.yasmingv.demoparkapi.repository;
+
+import com.yasmingv.demoparkapi.entity.ClienteVaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteVagaRepository extends JpaRepository<ClienteVaga, Long> {
+
+}
---
+++ src/main/java/com/yasmingv/demoparkapi/service/ClienteVagaService.java
@@ -0,0 +1,19 @@
+package com.yasmingv.demoparkapi.service;
+
+import com.yasmingv.demoparkapi.entity.ClienteVaga;
+import com.yasmingv.demoparkapi.repository.ClienteVagaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class ClienteVagaService {
+
+    private final ClienteVagaRepository repository;
+
+    @Transactional
+    public ClienteVaga salvar(ClienteVaga clienteVaga) {
+        return repository.save(clienteVaga);
+    }
+}
