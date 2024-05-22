c33eb9762656fcbf5df4c4e66c7d4bd69b03dc47
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-22 14:13:40 -0300

    Vagas - dto's, mapper

---
+++ src/main/java/com/yasmingv/demoparkapi/web/dto/mapper/VagaMapper.java
@@ -0,0 +1,20 @@
+package com.yasmingv.demoparkapi.web.dto.mapper;
+
+import com.yasmingv.demoparkapi.entity.Vaga;
+import com.yasmingv.demoparkapi.web.dto.VagaCreateDto;
+import com.yasmingv.demoparkapi.web.dto.VagaResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class VagaMapper {
+
+    public static Vaga toVaga(VagaCreateDto dto) {
+        return new ModelMapper().map(dto, Vaga.class);
+    }
+
+    public static VagaResponseDto toDto(Vaga vaga) {
+        return new ModelMapper().map(vaga, VagaResponseDto.class);
+    }
+}
