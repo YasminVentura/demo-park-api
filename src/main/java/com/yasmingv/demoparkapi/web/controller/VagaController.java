a63c97cd91d91c3bffc72e5fd4242a1bdf1e87f8
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-22 14:47:25 -0300

    Vagas - controller

---
+++ src/main/java/com/yasmingv/demoparkapi/web/controller/VagaController.java
@@ -0,0 +1,54 @@
+package com.yasmingv.demoparkapi.web.controller;
+
+import com.yasmingv.demoparkapi.entity.Vaga;
+import com.yasmingv.demoparkapi.service.VagaService;
+import com.yasmingv.demoparkapi.web.dto.VagaCreateDto;
+import com.yasmingv.demoparkapi.web.dto.VagaResponseDto;
+import com.yasmingv.demoparkapi.web.dto.mapper.VagaMapper;
+import com.yasmingv.demoparkapi.web.exception.ErrorMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("api/v1/vagas")
+public class VagaController {
+
+    private final VagaService vagaService;
+
+
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> create(@RequestBody @Valid VagaCreateDto dto) {
+        Vaga vaga = VagaMapper.toVaga(dto);
+        vagaService.salvar(vaga);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequestUri().path("/{codigo}")
+                .buildAndExpand(vaga.getCodigo())
+                .toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+
+    @GetMapping("/{codigo}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<VagaResponseDto> getByCodigo(@PathVariable String codigo) {
+        Vaga vaga = vagaService.buscarPorCodigo(codigo);
+        return ResponseEntity.ok(VagaMapper.toDto(vaga));
+    }
+
+}
