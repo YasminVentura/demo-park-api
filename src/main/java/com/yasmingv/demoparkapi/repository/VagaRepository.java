7c90a32a53e688fdf201e67f4eeacb2a70d8c227
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-22 14:07:09 -0300

    Vagas - classe, repositorio

---
+++ src/main/java/com/yasmingv/demoparkapi/repository/VagaRepository.java
@@ -0,0 +1,7 @@
+package com.yasmingv.demoparkapi.repository;
+
+import com.yasmingv.demoparkapi.entity.Vaga;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VagaRepository extends JpaRepository<Vaga, Long> {
+}
