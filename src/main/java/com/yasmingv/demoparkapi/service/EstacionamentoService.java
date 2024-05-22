a98c5759e88c70d16883004254b7e838413c50f5
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-22 16:29:28 -0300

    EsctacionamentoService - checkIn

---
+++ src/main/java/com/yasmingv/demoparkapi/service/EstacionamentoService.java
@@ -0,0 +1,37 @@
+package com.yasmingv.demoparkapi.service;
+
+import com.yasmingv.demoparkapi.entity.Cliente;
+import com.yasmingv.demoparkapi.entity.ClienteVaga;
+import com.yasmingv.demoparkapi.entity.Vaga;
+import com.yasmingv.demoparkapi.util.EstacionamentoUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class EstacionamentoService {
+
+    private final ClienteVagaService clienteVagaService;
+    private final ClienteService clienteService;
+    private final VagaService vagaService;
+
+    @Transactional
+    public ClienteVaga checkIn(ClienteVaga clienteVaga) {
+        Cliente cliente = clienteService.buscarPorCpf(clienteVaga.getCliente().getCpf());
+        clienteVaga.setCliente(cliente);
+
+        Vaga vaga = vagaService.buscarPorVagaLivre();
+        vaga.setStatus(Vaga.StatusVaga.OCUPADA);
+        clienteVaga.setVaga(vaga);
+
+        clienteVaga.setDataEntrada(LocalDateTime.now());
+
+        clienteVaga.setRecibo(EstacionamentoUtils.gerarRecibo());
+
+        return clienteVagaService.salvar(clienteVaga);
+    }
+
+}
