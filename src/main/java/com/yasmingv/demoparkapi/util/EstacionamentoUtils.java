10be96e232c6ec9ab1bc84700d511868b72bb97e
Yasmin Ventura <yasmin.gomsv@gmail.com>
2024-05-23 09:32:44 -0300

    EstacionamentoUtils - calcularCusto

--- src/main/java/com/yasmingv/demoparkapi/util/EstacionamentoUtils.java
+++ src/main/java/com/yasmingv/demoparkapi/util/EstacionamentoUtils.java
@@ -3,10 +3,38 @@ package com.yasmingv.demoparkapi.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EstacionamentoUtils {
+
+    private static final double PRIMEIROS_15_MINUTES = 5.00;
+    private static final double PRIMEIROS_60_MINUTES = 9.25;
+    private static final double ADICIONAL_15_MINUTES = 1.75;
+
+    public static BigDecimal calcularCusto(LocalDateTime entrada, LocalDateTime saida) {
+        long minutes = entrada.until(saida, ChronoUnit.MINUTES);
+        double total = 0.0;
+
+        if (minutes <= 15) {
+            total = PRIMEIROS_15_MINUTES;
+        } else if (minutes <= 60) {
+            total = PRIMEIROS_60_MINUTES;
+        } else {
+            long addicionalMinutes = minutes - 60;
+            Double totalParts = ((double) addicionalMinutes / 15);
+            if (totalParts > totalParts.intValue()) {
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * (totalParts.intValue() + 1));
+            } else { 
+                total += PRIMEIROS_60_MINUTES + (ADICIONAL_15_MINUTES * totalParts.intValue());
+            }
+        }
+
+        return new BigDecimal(total).setScale(2, RoundingMode.HALF_EVEN);
+    }
 
     public static String gerarRecibo() {
         LocalDateTime date = LocalDateTime.now();
